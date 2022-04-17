4534f3875883c1af2745e8822c09df3494e65e38
Revenant <dasdev.pratik@gmail.com>
2022-04-17 11:53:25 +0530

    experimenting-with-classes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      anecdotes: [],
+      current: 0
+    };
+  }
+
+  componentDidMount = () => {
+    axios.get("http://localhost:8001/anecdotes").then((response) => {
+      this.setState({ anecdotes: response.data });
+    });
+  };
+
+  handleClick = () => {
+    const current = Math.floor(Math.random() * this.state.anecdotes.length);
+    this.setState({ current });
+  };
+
+  render() {
+    if (this.state.anecdotes.length === 0) {
+      return <>no anecdotes</>;
+    }
+    return (
+      <div>
+        <h1>anecdote of the day</h1>
+        <div>{this.state.anecdotes[this.state.current].content}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
